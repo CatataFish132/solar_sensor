53d14c4009335d40da9fb9fa4c22ad46a0406ba4
CatataFish132 <thimoveldema@gmail.com>
2022-01-17 13:27:08 +0100

    final

--- boot.py
+++ boot.py
@@ -1,7 +1,7 @@
-import network, esp, gc, BME280, ujson
-import usocket as socket
+import network, esp, gc, BME280, ujson, time, ntptime, urequests, utime
+import socket
 from time import sleep
-from machine import Pin, I2C
+from machine import Pin, I2C, ADC, Timer, RTC
 
 esp.osdebug(None)
 gc.collect()
@@ -10,8 +10,10 @@ gc.collect()
 with open("config.json", "r") as f:
   config = ujson.loads(f.read())
 
+# setup i2c
 i2c = I2C(0)
 
+# connect to the wlan
 station = network.WLAN(network.STA_IF)
 
 station.active(True)
@@ -22,3 +24,28 @@ while station.isconnected() == False:
 
 print('Connection successful')
 print(station.ifconfig())
+
+# set date and time
+url = "https://worldtimeapi.org/api/timezone/Europe/Amsterdam"
+
+response = urequests.get(url)
+
+if response.status_code == 200:
+  parsed = response.json()
+  datetime_str = str(parsed["datetime"])
+  year = int(datetime_str[0:4])
+  month = int(datetime_str[5:7])
+  day = int(datetime_str[8:10])
+  hour = int(datetime_str[11:13])
+  minute = int(datetime_str[14:16])
+  second = int(datetime_str[17:19])
+  subsecond = int(round(int(datetime_str[20:26]) / 10000))
+  rtc = RTC()
+        
+  # update internal RTC
+  rtc.datetime((year, month, day, 0, hour, minute, second, subsecond))
+
+# this always crashed the second time
+# ntptime.host = "1.europe.pool.ntp.org"
+# ntptime.settime()
+# print(time.localtime())
--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 class Program:
 
+  class SolarSensor:
+
+    def __init__(self):
+      # setup Analogue Digital Converter
+        self.adc = ADC(Pin(34))
+        self.adc.atten(ADC.ATTN_0DB)
+        self.adc.width(ADC.WIDTH_12BIT)
+
+    # calculate Netto sun power
+    def read_netto(self):
+      return ((self.read_voltage()-(config["offset"]))/config["gain"])/(config["solar_panel_value"])
+
+    # calculate gross sun power
+    def read_gross(self):
+      bme = BME280.BME280(i2c=i2c)
+      diff_t = float(bme.temperature.strip("C"))-25
+      netto = self.read_netto()
+      return netto - netto*0.004*diff_t
+
+    # read solar sensor voltage
+    def read_voltage(self):
+      value = 0
+      # measures the voltage 200 times and takes the average to combat noise
+      for i in range(200):
+        value += self.adc.read()
+      if value/200 < 1:
+        return 0
+      else:
+        return 1.2*((value/200)/4096)+0.0650
+
   def __init__(self):
+    # setup socket for webpage
     self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.s.bind(('', 80))
     self.s.listen(5)
+    # create solarsensor object
+    self.solar_sensor = self.SolarSensor()
+    # set timer to log the values every 1 second
+    self.timer = Timer(-1)
+    self.timer.init(period=1000, mode=Timer.PERIODIC, callback=self.log_values)
+    # begin the webpage loop
     self.loop()
 
   def web_page(self):
+    # read bme sensor
     bme = BME280.BME280(i2c=i2c)
-    
+    # html with sensor readings
     html = """<html><head><meta http-equiv="refresh" content="1"><meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="data:,"><style>body { text-align: center; font-family: "Trebuchet MS", Arial;}
     table { border-collapse: collapse; width:35%; margin-left:auto; margin-right:auto; }
@@ -19,17 +57,37 @@ class Program:
     .sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px;
     </style></head><body><h1>ESP with BME280</h1>
     <table><tr><th>MEASUREMENT</th><th>VALUE</th></tr>
+    <tr><td>Solar Power Netto</td><td><span class="sensor">""" + str(self.solar_sensor.read_netto()) + """</span></td></tr>
+    <tr><td>Solar Power Gross</td><td><span class="sensor">""" + str(self.solar_sensor.read_gross()) + """</span></td></tr>
+    <tr><td>Date</td><td><span class="sensor">""" + str(time.localtime()) + """</span></td></tr>
+    <tr><td>voltage</td><td><span class="sensor">""" + str(self.solar_sensor.read_voltage())+ """</span></td></tr>
+    <tr><td>adc</td><td><span class="sensor">""" + str(self.solar_sensor.adc.read()) + """</span></td></tr>
     <tr><td>Temp. Celsius</td><td><span class="sensor">""" + str(bme.temperature) + """</span></td></tr>
-    <tr><td>Temp. Fahrenheit</td><td><span class="sensor">""" + str(round((bme.read_temperature()/100.0) * (9/5) + 32, 2))  + """F</span></td></tr>
     <tr><td>Pressure</td><td><span class="sensor">""" + str(bme.pressure) + """</span></td></tr>
     <tr><td>Humidity</td><td><span class="sensor">""" + str(bme.humidity) + """</span></td></tr></body></html>"""
     return html
 
+  def log_values(self, timer):
+    # read bme
+    bme = BME280.BME280(i2c=i2c)
+    # read solar sensor
+    solar = int(self.solar_sensor.read_gross())
+    temp = bme.temperature
+    # read date
+    year, month, mday, hour, minute, second, weekday, yearday = time.localtime()
+    # add solar sensor temperature and date into 1 string
+    log_str = ",".join((str(solar), str(temp), f"{year}-{yearday}-{hour}:{minute}:{second}"))
+    # write this string to the log.txt file
+    with open("log.txt", "a") as f:
+      f.write(log_str + "\n")
+
   def loop(self):
     while True:
       try:
+        # garbage collection
         if gc.mem_free() < 102000:
           gc.collect()
+        # accept connection
         conn, addr = self.s.accept()
         conn.settimeout(3.0)
         print('Got a connection from %s' % str(addr))
@@ -37,6 +95,7 @@ class Program:
         conn.settimeout(None)
         request = str(request)
         print('Content = %s' % request)
+        # send response
         response = self.web_page()
         conn.send('HTTP/1.1 200 OK\n')
         conn.send('Content-Type: text/html\n')
